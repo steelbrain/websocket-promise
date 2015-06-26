a6caa056883132dff9f601a409f04d4bb8c2dbac
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-25 19:03:34 -0600

    :new: Add Server::Broadcast

--- Source/Server.js
+++ Source/Server.js
@@ -13,13 +13,21 @@ class Server extends EventEmitter{
     this.Connections = new WeakMap // this.Server.clients <--> this.Connections
   }
   _onConnection(Connection){
-    let ClientConnection = new Client(Connection)
+    let ClientConnection = new Client(Connection, this)
     this.Connections.set(Connection, ClientConnection)
     this.emit('connection', ClientConnection)
   }
   onConnection(Callback){
     this.on('connection', Callback)
     return this
+  }
+  Broadcast(Type, Message){
+    for(let Connection of this.Server.clients){
+      let ClientConnection = this.Connections.get(Connection)
+      if(ClientConnection) { // Of course it's not undefined but still
+        ClientConnection.Send(Type, Message)
+      }
+    }
   }
 }
 
