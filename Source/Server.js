8bed78ee5911f2d8324e6514abe8554b55853a6d
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-25 18:21:53 -0600

    :art: Consume Client in Server

--- Source/Server.js
+++ Source/Server.js
@@ -3,17 +3,23 @@
 let EventEmitter = require('events').EventEmitter
 let WS = require('ws')
 
+let Client = require('./Client')
+
 class Server extends EventEmitter{
   constructor(Options){
     super()
     this.Server = WS.createServer(Options)
-    this.Server.on('connection', _onConnection.bind(this))
+    this.Server.on('connection', this._onConnection.bind(this))
+    this.Connections = new WeakMap // this.Server.clients <--> this.Connections
   }
   _onConnection(Connection){
-    console.log("Client Connected")
+    let ClientConnection = new Client(Connection)
+    this.Connections.set(Connection, ClientConnection)
+    this.emit('connection', ClientConnection)
   }
   onConnection(Callback){
     this.on('connection', Callback)
+    return this
   }
 }
 
