d180076fa300b41243b2f47b2984e9118dee03ae
steelbrain <aneesiqbalbhatti@gmail.com>
2015-08-22 16:24:50 -0600

    :new: Use sb-communication in WebSocketP

--- Browser/WebSocketP.js
+++ Browser/WebSocketP.js
@@ -1,49 +1,36 @@
 "use strict"
+
 // @Compiler-Compress "true"
 // @Compiler-Transpile "true"
+// @Compiler-Browserify "true"
 // @Compiler-Output "WebSocketP.min.js"
-class WebSocketP extends EventEmitter{
+
+const WebSocketPCommunication = require('sb-communication')
+
+class WebSocketP {
   constructor(Options){
-    super()
     let Me = this
+    this.Communication = new WebSocketPCommunication(false)
     this.Connection = new WebSocket(Options)
+    this.sendCallback = function(message) {
+      Me.Connection.send(JSON.stringify(message))
+    }
     this.Connection.addEventListener('message', function(Data){
       try {
         Data = JSON.parse(Data.data)
-        if(!Data.EXCHANGE) throw null
       } catch(err){
         return Me.emit('ParseError', Data)
       }
-      if(Data.Type === 'Request'){
-        Data.Result = null
-        Me.emit(Data.SubType, Data.Message, Data)
-        Me.emit('All', Data.Message, Data)
-      } else if(Data.Type === 'Broadcast'){
-        Me.emit(Data.SubType, Data.Message, Data)
-        Me.emit('All', Data.Message, Data)
-      } else if (Data.Type === 'Reply'){
-        Me.emit(`JOB:${Data.ID}`, Data.Message)
-      }
+      Me.Communication.gotMessage(Me.sendCallback, Data)
     })
   }
-  Send(Type, Message){
-    Message = Message || ''
-    this.Connection.send(JSON.stringify({Type: 'Broadcast', SubType: Type, Message: Message, EXCHANGE: true}))
-    return this
+  on(type, callback) {
+    return this.Communication.on(type, callback)
   }
-  Request(Type, Message){
-    Message = Message || ''
-    let Me = this
-    return new Promise(function(Resolve){
-      let JobID = (Math.random().toString(36)+'00000000000000000').slice(2, 7+2)
-      Me.once(`JOB:${JobID}`, Resolve)
-      Me.Connection.send(JSON.stringify({Type: 'Request', SubType: Type, Message: Message, ID: JobID, EXCHANGE: true}))
-    })
+  request(Type, Message){
+    return this.Communication.request(this.sendCallback, Type, Message)
   }
-  Finished(Job){
-    this.Connection.send(JSON.stringify({Type: 'Reply', ID: Job.ID, Message: Job.Result, EXCHANGE: true}))
-  }
-  Terminate(){
+  terminate(){
     this.Connection.close()
   }
 }
